5357c6f30b0b150ad5f15416c96da8ff7496885d
ishikaamittal <80573726+ishikaamittal@users.noreply.github.com>
2021-11-14 00:01:56 +0530

    models

--- management/models.py
+++ management/models.py
@@ -1,8 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Student(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    college_id = models.IntegerField(unique=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    enrollment = models.CharField(unique=True, blank=True)
+    course = models.CharField(max_length=100, blank=True)
+    phone = models.CharField(max_length=10)
+    dob = models.DateField()
+    address = models.TextField()
+
+
+class Room(models.Model):
+    category = models.CharField()
+    rent = models.IntegerField()
+    student = models.ForeignKey(Profile, on_delete=models.PROTECT, related_name='student')
+
+
+class Payment(models.Model):
+    payment_mode = models.CharField()
+    reference_number = models.CharField()
+    status = models.CharField()
+
