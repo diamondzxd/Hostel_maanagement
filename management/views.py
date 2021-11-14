e25ef0e67acb3e42c5cd25ac3d8f79ac7ed4b366
ishikaamittal <80573726+ishikaamittal@users.noreply.github.com>
2021-11-14 23:18:30 +0530

    pta nh kya kardia

--- management/views.py
+++ management/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from management.forms import ProfileForm
 
 
 # Create your views here.
 def index(request):
     pass
+
+
+def UserProfile(request):
+    form = ProfileForm()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("error")
+        return render(request, '/templates/index.html', {'form': form})
